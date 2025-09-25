7161b2697697ef9d895e63ee02b0f756fd98c97f
Gabriel Baumann <gabrielgoncalves238@gmail.com>
2025-09-25 10:35:02 -0300

    Chore: General

--- themes/managment/card/start.php
+++ themes/managment/card/start.php
@@ -1,5 +1,65 @@
 <?php $this->layout("layout"); ?>
-<div class="flex h-screen">
-    <!-- lateral aside -->
-    <div class="">teste</div>
+<div class="flex flex-col h-screen w-screen md:w-auto">
+
+    <header class="p-4 px-6 flex justify-start border-b border-gray-200 mt-8 md:mt-0">
+        <h1 class="font-semibold text-black">Cartão Alimentação</h1>
+    </header>
+
+    <header class="w-screen md:w-auto border-b border-gray-200 md:flex md:justify-start mt-6 md:mt-0">
+        <div class="flex flex-col md:flex-row px-6"> 
+            <a href="" class="py-2 px-6 cursor-pointer font-semibold border-b-2 border-green-800 duration-all transition-300 w-full md:w-auto text-green-800">Novo</a>
+            <a href="" class="py-2 px-6 cursor-pointer text-gray-600 font-semibold duration-all transition-300 w-full md:w-auto">Emergencial</a>
+            <a href="" class="py-2 px-6 cursor-pointer text-gray-600 font-semibold duration-all transition-300 w-full md:w-auto">2° Via</a>
+            <a href="" class="py-2 px-6 cursor-pointer text-gray-600 font-semibold duration-all transition-300 w-full md:w-auto">Recarga</a>
+            <a href="" class="py-2 px-6 cursor-pointer text-gray-600 font-semibold duration-all transition-300 w-full md:w-auto">Recarga extra</a> 
+        </div>
+    </header>
+
+    <header class="w-screen md:w-auto border-b border-gray-200 md:flex md:justify-start mt-6 md:mt-0">
+        <div class="flex flex-col md:flex-row px-6"> 
+            <a href="" class="py-2 px-4 cursor-pointer font-semibold border-b-2 border-green-800 duration-all transition-300 w-full md:w-auto text-sm text-green-800">Solicitados</a>
+            <a href="" class="py-2 px-4 cursor-pointer text-gray-600 font-semibold duration-all transition-300 w-full md:w-auto text-sm">Enviados</a>
+            <a href="" class="py-2 px-4 cursor-pointer text-gray-600 font-semibold duration-all transition-300 w-full md:w-auto text-sm">Recarga</a>
+            <a href="" class="py-2 px-4 cursor-pointer text-gray-600 font-semibold duration-all transition-300 w-full md:w-auto text-sm">Carregados</a>
+        </div>
+    </header>
+
+    <header class="flex flex-col gap-4 md:gap-0 md:flex-row md:items-center md:justify-between border-b border-gray-200 p-6">
+        <div class="flex items-center gap-2 w-[700px]">
+            <input type="text" class="p-2 px-4 bg-white w-full rounded-full border border-gray-300" placeholder="Pesquise por beneficiários. Ex:João Da Silva...">
+            <button class="border rounded-full p-2 bg-black text-white cursor-pointer">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                </svg>
+            </button>
+        </div>
+        <div class="">
+            <a href="" class="rounded-md bg-blue-500 text-white py-2 px-4 cursor-pointer font-semibold w-full">Novo cartão</a>
+        </div>
+    </header>
+    
+    <main class="p-6 h-full">
+        <div class="grid grid-cols-1 gap-6 md:grid-cols-3 h-full">
+            <div class="md:col-span-2 border border-gray-200">
+                
+                <!-- tabel stays here -->
+                <div>
+                    <title>Beneficiários</title>
+                    <table>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Nome</th>
+                        </tr>
+                        <tr>
+                            <td>Fulano de tal</td>
+                        </tr>
+                    </table>
+                </div>
+
+            </div>
+            <div class="md:col-span-1 bg-gray-100">
+                
+            </div>
+        </div>
+    </main>
 </div>
--- themes/managment/layout.php
+++ themes/managment/layout.php
@@ -23,7 +23,7 @@ use Soap\Url;
 <body>
     <div class="flex h-screen">
         <!-- lateral aside -->
-        <aside id="sidebar-main" class="relative hidden w-full md:flex flex-col justify-between md:w-[300px] md:min-w-[300px] md:max-w-[300px] p-6 overflow-y-auto">
+        <aside id="sidebar-main" class="bg-gray-100 border-r border-gray-200 relative hidden w-full md:flex flex-col justify-between md:w-[300px] md:min-w-[300px] md:max-w-[300px] p-6 overflow-y-auto">
             <button class="md:hidden absolute top-4 right-4" onclick="toggleMenu()">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
@@ -102,7 +102,7 @@ use Soap\Url;
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
                             </svg>
-                            <span class="menu" data-sidebar="emolumentos">Emolumentos</span>
+                            <span class="menu" data-sidebar="emolumentos">Documentação</span>
                         </a>
                         <a href="<?= url("/gas") ?>" class="menu flex items-center gap-2 px-3 py-2 border-l border-gray-300 hover:text-green-800 hover:border-green-800 flex items-center gap-2 transition-all duration-300 cursor-pointer text-gray-800 font-semibold">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
@@ -141,7 +141,7 @@ use Soap\Url;
         </aside>
         
         <!-- main area -->
-        <main id="main-area" class="relative h-screen md:w-full bg-gray-100">
+        <main id="main-area" class="relative h-screen md:w-full">
             <!-- mobile -->
             <?= $this->insert("/mobile/start") ?>
 
