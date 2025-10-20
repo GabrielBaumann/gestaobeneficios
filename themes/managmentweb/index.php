81f7156254e6503951cbff2ac3a9992853129f2d
Gabriel Baumann <gabrielgoncalves238@gmail.com>
2025-10-20 10:32:18 -0300

    Chore: unfinished initial login page

--- themes/managmentweb/index.php
+++ themes/managmentweb/index.php
@@ -15,22 +15,51 @@
     </style>
 </head>
 <body>
-    <div class="grid grid-cols-1 md:grid-cols-2 h-screen">
-
-        <div class="col-span-1 h-screen">
+    <div class="h-screen">
+        
+        <div class="w-full mx-auto md:w-2/5 2xl:w-2/7 p-12 flex flex-col justify-center">
+            <div class="text-center mb-10">
+                <img src="<?= theme("/img/logo.png")?>" alt="logo" class="h-[60px] mx-auto text-[#095998]">
+            </div>
             
-        </div>
-
-        <div class="hidden md:flex relative col-span-1">
-            <div class="h-screen overflow-hidden">
-
-                <img src="<?= theme("/img/semdes.jpg");?>" alt="semdes" class="w-full h-full object-cover object-[30%_center]">
-            </div>
-            <div class="">
+            <form class="space-y-6" action="<?= url("/") ?>" method="post">
+                <div><?= flash(); ?></div>
+                <?= csrf_input(); ?>
+                <div>
+                    <label for="cpf" class="block text-sm font-light text-gray-700 mb-1">CPF</label>
+                    <input 
+                        type="text"
+                        id="cpfuser"
+                        type="text"
+                        required
+                        name="cpfuser"
+                        placeholder="000.000.000-00"
+                        class="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-green-800 focus:border-green-800 outline-none transition duration-200 font-light"
+                    >
+                </div>
                 
-            </div>
+                <div>
+                    <label for="password" class="block text-sm font-light text-gray-700 mb-1">Senha</label>
+                    <input 
+                        id="password"
+                        type="password"
+                        required
+                        name="password"
+                        placeholder="********"
+                        class="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-green-800 focus:border-green-800 outline-none transition duration-200 font-light"
+                    >
+                </div>
+                
+                <div>
+                    <a 
+                        href="<?= url('/inicio'); ?>"
+                        class="cursor-pointer shadow-xl w-full flex justify-center py-3 px-4 border border-transparent rounded-full md:rounded-lg md:shadow-sm text-md font-light text-white bg-green-800 hover:bg-green-900 transition duration-200"
+                    >
+                        Entrar
+                    </a>
+                </div>
+            </form>
         </div>
-
+        
     </div>
-</body>
 </html>
