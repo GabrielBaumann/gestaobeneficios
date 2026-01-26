8975e984e9810f118245bb8adf075b0d54f82c6a
Gabriel Baumann <gabrielgoncalves238@gmail.com>
2026-01-26 15:46:29 -0300

    Fix: Second Card forms

--- themes/managment/card/formSecondCard.php
+++ themes/managment/card/formSecondCard.php
@@ -3,8 +3,8 @@
     <form action="<?= url("/cartao/solicitarsegundaviacartao") ?>" method="post" class="w-full p-4 flex flex-col gap-12">
     <?= csrf_input(); ?>
         <div class="flex flex-col gap-2">
-            <h1 class="text-xl font-semibold">Solicitação 2º Via de Cartão</h1>
-            <p>Preencha as seguintes informações</p>
+            <h1 class="text-xl font-semibold text-white">Solicitação 2º Via de Cartão</h1>
+            <p class="text-white">Preencha as seguintes informações</p>
             <span class="bg-gray-200 h-[3px] w-full"></span>
         </div>
 
@@ -12,10 +12,10 @@
             <!-- in this div i can put many inputs i want that appears side to side on desktop -->
             <div class="flex flex-col md:flex-row w-full gap-4">
                 <div class="flex flex-col w-full">
-                    <label for="bene" class="text-gray-800 font-semibold">CPF</label>
-                    <div class="flex gap-2">
-                        <input type="text" class="w-[300px] border border-gray-300 p-2 rounded-xs">
-                        <button class="cursor-pointer bg-gray-100 p-2 rounded-md hover:bg-gray-200">
+                    <label for="bene" class="text-white font-semibold">CPF</label>
+                    <div class="flex items-center">
+                        <input type="text" class="w-[300px] bg-gray-700 text-white p-2 rounded-l-xs">
+                        <button class="cursor-pointer bg-gray-700 p-2 rounded-r-full text-white cursor-pointer">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                             </svg>
@@ -26,8 +26,8 @@
 
             <div class="flex flex-col md:flex-row w-full gap-4">
                 <div class="flex flex-col w-full">
-                    <label for="person-benefit" class="text-gray-800 font-semibold">Beneficiário *</label>
-                        <select name="person-benefit" id="person-benefit" class="w-full border border-gray-300 p-2 rounded-xs">
+                    <label for="person-benefit" class="text-white font-semibold">Beneficiário *</label>
+                        <select name="person-benefit" id="person-benefit" class="w-full bg-gray-700 text-white p-2 rounded-xs">
                             <option value="">Selecione</option>
                             <?php foreach($personbenefit as $personbenefititem): ?>    
                                 <option value="<?= $personbenefititem->id_person_benefit; ?>"><?= $personbenefititem->name_benefit; ?></option>
@@ -38,8 +38,8 @@
             
             <div class="flex flex-col md:flex-row w-full gap-4">
                 <div class="flex flex-col w-full">
-                    <label for="technician" class="text-gray-800 font-semibold">Técnico(a) *</label>
-                    <select type="text" name="technician" id="technician" placeholder="Técnico" class="w-full border border-gray-300 p-2 rounded-xs">
+                    <label for="technician" class="text-white font-semibold">Técnico(a) *</label>
+                    <select type="text" name="technician" id="technician" placeholder="Técnico" class="w-full bg-gray-700 text-white p-2 rounded-xs">
                         <option value="">Selecione</option>
                         <option value="1">LUCAS DOS SANTOS SILVA</option>
                         <option value="2">ALDENORA BAIA</option>
@@ -49,8 +49,8 @@
                 </div>
 
                 <div class="flex flex-col w-full">
-                    <label for="date-request" class="text-gray-800 font-semibold">Data da Solicitação *</label>
-                    <input type="date" name="date-request" id="date-request" placeholder="data" class="w-full border border-gray-300 p-2 rounded-xs">
+                    <label for="date-request" class="text-white font-semibold">Data da Solicitação *</label>
+                    <input type="date" name="date-request" id="date-request" placeholder="data" class="w-full bg-gray-700 text-white p-2 rounded-xs">
                 </div>
             </div>
 
