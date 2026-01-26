c8d58f0fecc4806994580661bd558c5dc67c0657
Gabriel Baumann <gabrielgoncalves238@gmail.com>
2026-01-26 11:33:57 -0300

    Fix try: All card front end

--- themes/managment/card/listRechargeExtract.php
+++ themes/managment/card/listRechargeExtract.php
@@ -1,6 +1,6 @@
 <?php if (!empty($recharge)): ?>
 
-    <table class="w-full">
+    <table class="w-full bg-white/10 rounded-sm">
         <thead class="">
             <tr>
                 <th class="py-3 px-4 text-left text-sm font-medium text-white uppercase tracking-wider">Id_Recarga</th>
@@ -11,31 +11,31 @@
             </tr>
         </thead>
         <?php $count = 1; ?>
-        <tbody class="divide-y divide-gray-200">
+        <tbody class="divide-y divide-gray-600">
             <?php if (!empty($recharge)): ?>
                 <?php foreach($recharge as $item):?>
-                    <!-- <tr onclick="showRecharges()"  class="hover:bg-gray-50 transition-colors"> -->
-                    <td class="py-3 px-4 text-sm text-gray-800"><?= $item->id_card_recharge; ?></td>
-                    <td class="py-3 px-4 text-sm text-gray-800"><?= fncstr_price($item->value); ?></td>
-                    <td class="py-3 px-4 text-sm text-gray-600">
-                        <?= fncMonthString($item->month_recharge); ?>
+                    <tr onclick="showRecharges()"  class="hover:bg-gray-800 transition-colors cursor-pointer transition-all duration-300">
+                        <td class="py-3 px-4 text-sm text-white"><?= $item->id_card_recharge; ?></td>
+                        <td class="py-3 px-4 text-sm text-white"><?= fncstr_price($item->value); ?></td>
+                        <td class="py-3 px-4 text-sm text-white">
+                            <?= fncMonthString($item->month_recharge); ?>
 
-                        <?php  if($item->type_request === "recarga extra"): ?>
-                            <?= $item->type_request; ?>
-                        <?php endif; ?>
-                        
-                    </td>
-                    <td class="py-3 px-4 text-center"><?= $item->year_recharge; ?></td>
-                    <td class="py-3 px-4 text-center">
-                        <span class="status px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                            <?= $item->status_recharge; ?>
-                        </span>
-                    </td>
+                            <?php  if($item->type_request === "recarga extra"): ?>
+                                <?= $item->type_request; ?>
+                            <?php endif; ?>
+                            
+                        </td>
+                        <td class="py-3 px-4 text-center text-white"><?= $item->year_recharge; ?></td>
+                        <td class="py-3 px-4 text-center text-white">
+                            <span class="status px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                <?= $item->status_recharge; ?>
+                            </span>
+                        </td>
                     </tr>
                 <?php endforeach;?>
             <?php else:?>
                 <tr>
-                    <td colspan="6" class="py-3 px-4 text-center text-gray-500">
+                    <td colspan="6" class="py-3 px-4 text-center text-white">
                         Não há dados para exibir.
                     </td>
                 </tr>
@@ -47,7 +47,7 @@
     <div>Não há dados.</div>
 <?php endif; ?>
 
-<footer class="border-t border-gray-300 flex items-center justify-start gap-3 pt-3">
-    <h1 class="font-semibold text-gray-800 text-xl">Saldo atual:</h1>
-    <p class="text-gray-500 text-xl"><?= fncstr_price($balance->value ?? 00); ?></p>
+<footer class="flex items-center justify-start gap-3 pt-3">
+    <h1 class="font-semibold text-white text-xl">Saldo atual:</h1>
+    <p class="text-white text-xl"><?= fncstr_price($balance->value ?? 00); ?></p>
 </footer>
